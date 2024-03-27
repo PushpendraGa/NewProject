05ac509f148cc751ba833f960f227c4fcf128c59
pushpendra <pkgaur8085@gmai.com>
2024-03-27 01:11:06 -0700

    Name

--- src/main/java/PageClass/Broswer.java
+++ src/main/java/PageClass/Broswer.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Broswer {
 
 	
+	
 	public static void main(String[] args) throws InterruptedException {
 		
 		
@@ -20,7 +21,12 @@ public class Broswer {
 			driver.manage().window().maximize();
 			driver.findElement(By.xpath("//input[@id=\"alert1\"]")).click();
 			Thread.sleep(2000);
-			driver.switchTo().alert().accept();
+			//when we accept the alert
+			driver.switchTo().alert().accept(); 
+			//sends data
+			driver.switchTo().alert().sendKeys("name");
+			// alert dismiss
+			driver.switchTo().alert().dismiss();
 		
 		
 		
--- src/main/java/PageClass/Dropdown.java
+++ src/main/java/PageClass/Dropdown.java
@@ -1,5 +1,31 @@
 package PageClass;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 public class Dropdown {
+	
+@Test
+	public void dropdown() throws InterruptedException {		
+		
+	      String path = "C:\\Repo\\Pushpendra\\NewProject\\Driver\\chromedriver.exe";
+			
+			System.setProperty("webdriver.chrome.driver", path);
+			
+			ChromeDriver driver = new ChromeDriver();
+			//driver.get("https://www.google.com/");
+			driver.get("https://omayo.blogspot.com/");
+			driver.manage().window().maximize();
+			
+			
 
+	
+}	
+	
+	
+	
 }
+
+
+
+
+
